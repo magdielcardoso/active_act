000024e7dd6e7b9de8f0e47bf058af0d732c4257
Magdiel Cardoso <dzmartins.contato@icloud.com>
2025-06-11 14:03:05 -0300

    Add retry mechanism to ApplicationAction for error handling with configurable attempts and wait time

--- lib/active_act/application_action.rb
+++ lib/active_act/application_action.rb
@@ -22,6 +22,10 @@ module ActiveAct
         @act_as_type = type
       end
 
+      def retry_on(error_class, attempts: 3, wait: 0)
+        @retry_on_config = { error_class: error_class, attempts: attempts, wait: wait }
+      end
+
       def _before_call_callbacks
         superclass.respond_to?(:_before_call_callbacks) ? (superclass._before_call_callbacks + (@before_call_callbacks || [])) : (@before_call_callbacks || [])
       end
@@ -37,6 +41,10 @@ module ActiveAct
       def _act_as_type
         @act_as_type || (superclass.respond_to?(:_act_as_type) ? superclass._act_as_type : nil)
       end
+
+      def _retry_on_config
+        @retry_on_config || (superclass.respond_to?(:_retry_on_config) ? superclass._retry_on_config : nil)
+      end
     end
 
     def self.call(*args, as_job: true, **kwargs, &block)
@@ -45,7 +53,13 @@ module ActiveAct
         ActiveAct::ActionResult.new({ enqueued: true, action: name, args: args, kwargs: kwargs })
       else
         instance = new
+        retry_config = _retry_on_config
+        attempts = retry_config ? retry_config[:attempts] : 1
+        wait = retry_config ? retry_config[:wait] : 0
+        error_class = retry_config ? retry_config[:error_class] : nil
+        tries = 0
         begin
+          tries += 1
           _before_call_callbacks.each { |cb| instance.send(cb, *args, **kwargs) }
           instance.before_call(*args, **kwargs) if instance.respond_to?(:before_call)
           result = instance.call(*args, **kwargs, &block)
@@ -57,6 +71,10 @@ module ActiveAct
             ActiveAct::ActionResult.new(result)
           end
         rescue StandardError => e
+          if retry_config && e.is_a?(error_class) && tries < attempts
+            sleep(wait) if wait.to_f > 0
+            retry
+          end
           _on_error_callbacks.each { |cb| instance.send(cb, e) }
           instance.on_error(e) if instance.respond_to?(:on_error)
           ActiveAct::ActionResult.new(nil, error: e)
